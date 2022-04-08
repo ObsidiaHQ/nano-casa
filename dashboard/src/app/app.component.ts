754b5c3f74e9563bad3d26ebb75e41a0baf6fa6c
George Mamar <george@mamar.dev>
2022-04-08 11:20:02 +0200

    show individual repos for each user

--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -3,6 +3,31 @@ import { HttpClient } from '@angular/common/http';
 import { faCodeBranch, faHistory, faInfoCircle, faStar, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { ScaleType } from '@swimlane/ngx-charts';
 
+interface Repo {
+    name:             string,
+    full_name:        string,
+    created_at:       string,
+    stargazers_count: number
+}
+interface Commit {
+    count:            number, 
+    date:             string
+}
+interface Contributor {
+    login:            string,
+    avatar_url:       string,
+    contributions:    number,
+    repos:            string[],
+    repos_count:      number
+}
+interface Misc {
+    protocol_milestone: {
+        title:         string,
+        open_issues:   number,
+        closed_issues: number
+    }
+}
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -17,23 +42,21 @@ export class AppComponent implements OnInit {
     faInfo = faInfoCircle;
 
     reposData = [];
-    popularRepos = [];
-    popularReposPage = [];
-    contributorsPage = [];
+    popularRepos: Repo[] = [];
+    popularReposPage: Repo[] = [];
+    contributorsPage: Contributor[] = [];
     contributorsPageIndex = 0;
 
-    data = {
-        repos: [],
-        contributors: [],
-        commits: [{name: 'commits', series: []}],
-        misc: {
-            protocol_milestone: {
-                title: '',
-                open_issues: 0,
-                closed_issues: 0
-            }
+    repos: Repo[] = [];
+    contributors: Contributor[] = [];
+    commits = [{name: 'commits', series: []}];
+    misc: Misc = {
+        protocol_milestone: {
+            title: '',
+            open_issues: 0,
+            closed_issues: 0
         }
-    };
+    }
 
     constructor(private http: HttpClient) { }
 
@@ -43,21 +66,15 @@ export class AppComponent implements OnInit {
 
     getData(): void {
         this.http.get('https://nano.casa/data').subscribe((data: any) => {
-            this.data.contributors = data.contributors.map(({ login, repos_count, avatar_url, repos, contributions }) => ({ 
-                login, 
-                repos_count, 
-                avatar_url, 
-                contributions,
-                repos: repos.slice(0, 3).join(", ") + (repos.length > 3 ? ` +${repos.length - 3} more` : '') 
-            }));
-            this.data.misc = data.misc;
+            this.contributors = data.contributors;
+            this.misc = data.misc;
             this.setRepos(data.repos);
             this.setCommits(data.commits);
         });
     }
 
     setRepos(repos: any[]) {
-        this.data.repos = repos;
+        this.repos = repos;
         this.popularRepos = [...repos].sort((a, b) => b.stargazers_count - a.stargazers_count);
 
         // list years since 2014
@@ -73,7 +90,7 @@ export class AppComponent implements OnInit {
         this.reposData = Object.values(YEARS_DICT);
     }
 
-    setCommits(commits: any[]) {
-        this.data.commits[0].series = commits.map((com) => ({ name: com.date, value: com.count }));
+    setCommits(commits: Commit[]) {
+        this.commits[0].series = commits.map((com) => ({ name: com.date, value: com.count }));
     }
 }
